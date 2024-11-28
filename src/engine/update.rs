2dc6c4f3b008788189511a753604de9c51a6faec
floatshark <x4samezame@gmail.com>
2024-11-29 04:39:55 +0900

    fix: add shift view control

--- src/engine/update.rs
+++ src/engine/update.rs
@@ -2,6 +2,7 @@ use crate::engine::define;
 use crate::frontend;
 use crate::rendering;
 
+use glam::Vec4Swizzles;
 use wasm_bindgen::JsCast;
 
 #[derive(Clone, Copy, Default)]
@@ -10,9 +11,9 @@ pub struct UpdateContext {
     pub eye_direction: glam::Vec3,
 }
 
-impl UpdateContext{
-    pub fn initial() -> UpdateContext{
-        UpdateContext{
+impl UpdateContext {
+    pub fn get_init() -> UpdateContext {
+        UpdateContext {
             eye_location: glam::Vec3 {
                 x: 3.0,
                 y: 0.0,
@@ -35,41 +36,51 @@ pub fn update(
         .get_element_by_id(define::CANVAS_ELEMENT_ID)
         .unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into().unwrap();
-
     let width: u32 = canvas.client_width() as u32;
     let height: u32 = canvas.client_height() as u32;
     let aspect_ratio: f32 = width as f32 / height as f32;
 
     // Calculate eye direction (rotation)
     let on_click: bool = mouse_event_js.get().on_click;
-    if on_click {
+    let on_shift: bool = mouse_event_js.get().on_shift;
+    if on_click && !on_shift {
         let rotate_x_mat =
-            glam::Mat3::from_rotation_z(-1.0 * mouse_event_js.get().movement_x as f32 * 0.01);
+            glam::Mat3::from_rotation_z(-1.0 * mouse_event_js.get().movement_x as f32 * 0.005);
         direction = rotate_x_mat.mul_vec3(direction);
 
         let y_axis = glam::vec3(direction.x, direction.y, direction.z)
             .cross(glam::vec3(0.0, 0.0, 1.0))
             .normalize();
-        let rotate_y_quat =
-            glam::Quat::from_axis_angle(y_axis, -1.0 * mouse_event_js.get().movement_y as f32 * 0.01);
+        let rotate_y_quat = glam::Quat::from_axis_angle(
+            y_axis,
+            -1.0 * mouse_event_js.get().movement_y as f32 * 0.005,
+        );
         direction = rotate_y_quat.mul_vec3(direction);
+    } else if on_click && on_shift {
+        let direction_mat: glam::Mat4 = glam::Mat4::from_translation(direction);
+        let up_move_vec: glam::Vec4 = direction_mat.mul_vec4(glam::Vec4::Z).normalize();
+        let right_move_vec: glam::Vec4 = direction_mat.mul_vec4(glam::Vec4::Y).normalize();
+        eye += -1.0 * up_move_vec.xyz() * mouse_event_js.get().movement_y as f32 * 0.01;
+        eye += 1.0 * right_move_vec.xyz() * mouse_event_js.get().movement_x as f32 * 0.01;
     }
+
     // Calculate eye location
     let on_wheel = mouse_event_js.get().on_wheel;
-    if on_wheel{
+    if on_wheel {
         eye += -1.0 * direction.normalize() * mouse_event_js.get().wheel_delta_y as f32 * 0.005;
     }
 
+    // Create matrices and write buffer
     let view_matrix = glam::Mat4::look_to_rh(eye, direction, glam::Vec3::Z);
     let projection_matrix: glam::Mat4 =
         glam::Mat4::perspective_rh(std::f32::consts::FRAC_PI_4, aspect_ratio, 0.0, 10.0);
-
     let mx_total: glam::Mat4 = projection_matrix * view_matrix;
     let mx_ref: &[f32; 16] = mx_total.as_ref();
     render_context
         .queue
         .write_buffer(&render_context.uniform_buf, 0, bytemuck::cast_slice(mx_ref));
 
+    // Update
     let view_temp: UpdateContext = UpdateContext {
         eye_location: eye,
         eye_direction: direction,
--- src/frontend/controls.rs
+++ src/frontend/controls.rs
@@ -9,6 +9,7 @@ pub struct MouseEventResponseJs {
     pub on_click: bool,
     pub wheel_delta_y: f64,
     pub on_wheel: bool,
+    pub on_shift: bool,
 }
 
 pub fn add_event_listener_control(
@@ -46,6 +47,30 @@ pub fn add_event_listener_control(
             event_response_clone_wheel.set(response);
         }) as Box<dyn FnMut(_)>);
 
+    let event_response_clone_key_down: std::rc::Rc<std::cell::Cell<MouseEventResponseJs>> =
+        event_response.clone();
+
+    let key_down_closure: wasm_bindgen::prelude::Closure<dyn FnMut(_)> =
+        wasm_bindgen::closure::Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+            let mut response: MouseEventResponseJs = event_response_clone_key_down.get();
+
+            response.on_shift = event.shift_key();
+
+            event_response_clone_key_down.set(response);
+        }) as Box<dyn FnMut(_)>);
+
+    let event_response_clone_key_up: std::rc::Rc<std::cell::Cell<MouseEventResponseJs>> =
+        event_response.clone();
+
+    let key_up_closure: wasm_bindgen::prelude::Closure<dyn FnMut(_)> =
+        wasm_bindgen::closure::Closure::wrap(Box::new(move |_event: web_sys::KeyboardEvent| {
+            let mut response: MouseEventResponseJs = event_response_clone_key_up.get();
+
+            response.on_shift = false;
+
+            event_response_clone_key_up.set(response);
+        }) as Box<dyn FnMut(_)>);
+
     canvas
         .add_event_listener_with_callback("mousemove", mouse_move_closure.as_ref().unchecked_ref())
         .unwrap();
@@ -55,4 +80,14 @@ pub fn add_event_listener_control(
         .add_event_listener_with_callback("wheel", mouse_wheel_closure.as_ref().unchecked_ref())
         .unwrap();
     mouse_wheel_closure.forget();
+
+    canvas
+        .add_event_listener_with_callback("keydown", key_down_closure.as_ref().unchecked_ref())
+        .unwrap();
+    key_down_closure.forget();
+
+    canvas
+        .add_event_listener_with_callback("keyup", key_up_closure.as_ref().unchecked_ref())
+        .unwrap();
+    key_up_closure.forget();
 }
